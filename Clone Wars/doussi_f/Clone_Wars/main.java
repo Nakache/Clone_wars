4df9442fb9fa956d63cc4f8ab145998a06ec3c5d
nakach_t <nakach_t@7b5cab19-d409-4866-8a26-1cda3f052dc4>
2016-02-26 15:57:03 +0000

    Last Commit of the day

--- Clone Wars/doussi_f/Clone_Wars/main.java
+++ Clone Wars/doussi_f/Clone_Wars/main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 /**
  * Created by Zouclar on 26/02/2016.
  */
-public static void main(String[]args){
+public class main {
+    public static void main(String[] args) {
         System.out.println("Veuillez Choisir une classe Pamis:");
+        System.out.println("- Jedi");
+        System.out.println("- Sith"); //BountyHunter, ImperialAgent, Mercenary, Jawa
+        System.out.println("- Bounty Hunter");
+        System.out.println("- Imperial Agent");
+        System.out.println("- Mercenary");
+        System.out.println("- Jawa");
 
+        Scanner scan = new Scanner(System.in);
+        String classe = scan.nextLine();
+        System.out.println("Veuillez Choisir un nom:");
+        String name = scan.nextLine();
+        System.out.println("Votre personnage s'apelle: " + name +" Il est de classe: " + classe);
+
+        try{
+            Personages perso = new Personages() {
+            };
         }
+        catch(Exception e){
+            System.out.println("Veuillez Entrer un nom valide");
+        }
+    }
+}
